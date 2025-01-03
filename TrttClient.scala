f6b74177b150a6a2b24f5e42e020301333760a5d
chihirobelmo <chihirobelmo@gmail.com>
2025-01-03 12:56:48 +0900

    update each 2 seconds

--- TrttClient.scala
+++ TrttClient.scala
@@ -1,9 +1,11 @@
+import akka.actor.ActorSystem
 import java.net.{Socket, InetSocketAddress}
 import java.io.{BufferedReader, InputStreamReader, PrintWriter}
 import scala.concurrent.{Future, ExecutionContext}
 import scala.util.{Failure, Success}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.collection.mutable.HashMap
 
 object ThreadState extends Enumeration {
     type ThreadState = Value
@@ -11,6 +13,7 @@ object ThreadState extends Enumeration {
 }
 
 class TrttClient {
+    implicit val system: ActorSystem = ActorSystem("PeriodicTaskSystem")
     @volatile private var connecting: Boolean = false
     @volatile private var connected: Boolean = false
     @volatile private var quit: Boolean = false
@@ -18,6 +21,31 @@ class TrttClient {
     private var server: Option[(String, Int)] = None
     private val numRetries: Int = 5
     private var retries: Int = 0
+    private val idMap = HashMap[String, Map[String, String]]()
+    private var timestamp: String = ""
+
+    startPeriodicTask(2.seconds)
+
+    private def startPeriodicTask(interval: FiniteDuration): Unit = {
+        system.scheduler.scheduleWithFixedDelay(0.seconds, interval) { () =>
+            {
+                idMap.foreach { case (id, attributes) =>
+                    // Extract specific attributes
+                    val t = attributes.getOrElse("T", "Unknown")
+                    val category = attributes.getOrElse("Category", "Unknown")
+                    val name = attributes.getOrElse("Name", "Unknown")
+                    val pilot = attributes.getOrElse("Pilot", "Unknown")
+                    val coalition = attributes.getOrElse("Coalition", "Unknown")
+                    val country = attributes.getOrElse("Country", "Unknown")
+                    val typeAttr = attributes.getOrElse("Type", "Unknown")
+                    val ias = attributes.getOrElse("IAS", "0").toDouble
+                    val throttle = attributes.getOrElse("Throttle", "0").toDouble
+
+                    println(s"ID: $id, T: $t, Category: $category, Name: $name, Pilot: $pilot, Coalition: $coalition, Country: $country, Type: $typeAttr, IAS: $ias, Throttle: $throttle")
+                }
+            }
+        }
+    }
 
     private def setStatus(state: ThreadState.ThreadState, message: String): Unit = {
         println(s"Status: $state, Message: $message")
@@ -59,8 +87,7 @@ class TrttClient {
     def parseLine(line: String): Unit = {
         // time stamp line: #123456.789
         if (line.startsWith("#")) {
-            val timestamp = line.stripPrefix("#").toDouble
-            println(s"Timestamp: $timestamp")
+            timestamp = line.stripPrefix("#").toString
         // ID and attributes line: 1,attr1=val1,attr2=val2
         } else {
             val parts = line.split(",")
@@ -71,18 +98,20 @@ class TrttClient {
                 key -> value
             }.toMap
 
-            // Extract specific attributes
-            val t = attributes.getOrElse("T", "Unknown")
-            val category = attributes.getOrElse("Category", "Unknown")
-            val name = attributes.getOrElse("Name", "Unknown")
-            val pilot = attributes.getOrElse("Pilot", "Unknown")
-            val coalition = attributes.getOrElse("Coalition", "Unknown")
-            val country = attributes.getOrElse("Country", "Unknown")
-            val typeAttr = attributes.getOrElse("Type", "Unknown")
-            val ias = attributes.getOrElse("IAS", "0").toDouble
-            val throttle = attributes.getOrElse("Throttle", "0").toDouble
+            idMap += (id -> (attributes + ("TimeStamp" -> timestamp)))
 
-            println(s"ID: $id, T: $t, Category: $category, Name: $name, Pilot: $pilot, Coalition: $coalition, Country: $country, Type: $typeAttr, IAS: $ias, Throttle: $throttle")
+            // // Extract specific attributes
+            // val t = attributes.getOrElse("T", "Unknown")
+            // val category = attributes.getOrElse("Category", "Unknown")
+            // val name = attributes.getOrElse("Name", "Unknown")
+            // val pilot = attributes.getOrElse("Pilot", "Unknown")
+            // val coalition = attributes.getOrElse("Coalition", "Unknown")
+            // val country = attributes.getOrElse("Country", "Unknown")
+            // val typeAttr = attributes.getOrElse("Type", "Unknown")
+            // val ias = attributes.getOrElse("IAS", "0").toDouble
+            // val throttle = attributes.getOrElse("Throttle", "0").toDouble
+
+            // println(s"ID: $id, T: $t, Category: $category, Name: $name, Pilot: $pilot, Coalition: $coalition, Country: $country, Type: $typeAttr, IAS: $ias, Throttle: $throttle")
         }
     }
 
--- build.sbt
+++ build.sbt
@@ -10,5 +10,7 @@ libraryDependencies ++= Seq(
   "org.lwjgl" % "lwjgl-glfw" % "3.3.1",
   "org.lwjgl" % "lwjgl" % "3.3.1" classifier "natives-windows", // natives-macos | natives-linux
   "org.lwjgl" % "lwjgl-opengl" % "3.3.1" classifier "natives-windows",
-  "org.lwjgl" % "lwjgl-glfw" % "3.3.1" classifier "natives-windows"
+  "org.lwjgl" % "lwjgl-glfw" % "3.3.1" classifier "natives-windows",
+  "com.typesafe.akka" %% "akka-actor-typed" % "2.6.18",
+  "com.typesafe.akka" %% "akka-stream" % "2.6.18"
 )
